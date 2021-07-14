4d24fa1975e124d0673033e3f17cef7de81c4b89
Eric Guzman <ericmguzmanc@gmail.com>
2021-07-14 17:09:24 -0400

    routing, main components, AuthGuard and some config changes

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,10 @@ export class NavComponent implements OnInit {
   model: any = {};
   currentUser$: Observable<User> = new Observable<User>();
 
-  constructor(private accountService: AccountService) { }
+  constructor(
+    private accountService: AccountService, 
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
@@ -21,14 +26,15 @@ export class NavComponent implements OnInit {
   login(): void {
     console.log(this.model);
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.error(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
